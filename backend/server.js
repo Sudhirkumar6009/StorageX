33ac28ffc99e0cb68bdc0b6d5e4c04d18da18391
Sudhirkumar6009 <sudhir.kuchara@gmail.com>
2025-06-24 17:33:30 +0530

    CORS

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,31 @@
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import mongoPing from './Mongo/mongoPing.js';
-import mongoProfile from './Profile/mongoProfile.js';
-import storePublicAddressMongo from './Mongo/storePublicAddressMongo.js';
-import fetchProfile from './Profile/fetchProfile.js';
-import listoutFiles from './Filebase/listoutFiles.js';
-import deleteFile from './Filebase/deleteFile.js';
-import uploadFile from './Filebase/uploadFile.js';
-import cidMatchingMongo from './Mongo/cidMatchingMongo.js';
-import googleAccount from './Signin/googleAccount.js';
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import mongoPing from "./Mongo/mongoPing.js";
+import mongoProfile from "./Profile/mongoProfile.js";
+import storePublicAddressMongo from "./Mongo/storePublicAddressMongo.js";
+import fetchProfile from "./Profile/fetchProfile.js";
+import listoutFiles from "./Filebase/listoutFiles.js";
+import deleteFile from "./Filebase/deleteFile.js";
+import uploadFile from "./Filebase/uploadFile.js";
+import cidMatchingMongo from "./Mongo/cidMatchingMongo.js";
+import googleAccount from "./Signin/googleAccount.js";
+import cors from "cors";
 
-dotenv.config({ path: './.env' });
+dotenv.config({ path: "./.env" });
 
 const app = express();
-
+app.use(
+  cors({
+    origin: [
+      "https://storage-x-lilac.vercel.app",
+      "http://localhost:8080",
+      "http://localhost:3000",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization", "x-access-token"],
+  })
+);
 const PORT = process.env.PORT || 3001;
 
 app.use(cors());
